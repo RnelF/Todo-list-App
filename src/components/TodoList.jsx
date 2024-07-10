10412a3138b9fce6c738d814426aeb986e51945e
Arnel Francia <nelffff@gmail.com>
2024-07-10 21:19:55 +0800

    update

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,10 @@
 import Todoitem from "./Todoitem.jsx";
 import style from "./todolist.module.css";
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, setTodos }) {
   return (
     <div className={style.list}>
       {todos.map((item) => (
-        <Todoitem key={item} item={item} />
+        <Todoitem key={item} item={item} todos={todos} setTodos={setTodos} />
       ))}
     </div>
   );
--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -1,11 +1,19 @@
 import styles from "./todoitem.module.css";
-export default function Todoitem({ item }) {
+export default function Todoitem({ item, todos, setTodos }) {
+  function handleDelete() {
+    setTodos(todos.filter((todo) => todo !== item));
+  }
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
         {item}
         <span>
-          <button className={styles.deleteButton}>x</button>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            x
+          </button>
         </span>
       </div>
 
