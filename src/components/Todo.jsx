92b788e8ae833af22abe6d2875faf0e3387b8f6c
Arnel Francia <nelffff@gmail.com>
2024-07-10 20:36:54 +0800

    update

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,30 +1,14 @@
 import { useState } from "react";
 import Todoitem from "./Todoitem.jsx";
+import Form from "./Form.jsx";
+import TodoList from "./TodoList.jsx";
 
 export default function Todo() {
-  const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    setTodos([...todos, todo]);
-    setTodo("");
-  }
-
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          onChange={(e) => setTodo(e.target.value)}
-          value={todo}
-          text="text"
-        />
-        <button type="submit">Add</button>
-      </form>
-
-      {todos.map((item) => (
-        <Todoitem key={item} item={item} />
-      ))}
+      <Form todos={todos} setTodos={setTodos} />
+      <TodoList todos={todos} />
     </div>
   );
 }
